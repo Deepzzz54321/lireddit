53ff2fa2e0e74670120ca829a0891299c5766564
eswarclynn <esuclintblazer@gmail.com>
2020-10-31 00:43:32 +0530

    User validation.

--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -1,6 +1,16 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 import { User } from "../entities/User";
 import { MyContext } from "../types";
+import argon2 from "argon2";
 
 @InputType()
 class UserCreateInput {
@@ -14,19 +24,121 @@ class UserCreateInput {
   password: string;
 }
 
+@InputType()
+class LoginInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
-export class HelloResolver {
-  @Mutation(() => User)
+export class UserResolver {
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UserCreateInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Length should be of minimum 3 characters",
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Length should be of minimum 4 characters",
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       email: options.email,
+      password: hashedPassword,
     });
-    await em.persistAndFlush(user);
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === "23505") {
+        let field = err.constraint.split("_")[1];
+        return {
+          errors: [
+            {
+              field,
+              message: `${field} already exists!`,
+            },
+          ],
+        };
+      }
+    }
 
-    return user;
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: LoginInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username,
+    });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username doesn't exist",
+          },
+        ],
+      };
+    }
+
+    const validPassword = await argon2.verify(user.password, options.password);
+    if (!validPassword) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Invalid password",
+          },
+        ],
+      };
+    }
+
+    return { user };
+  }
+
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
   }
 }
